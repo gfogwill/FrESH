4135c0e1cc38cc8e328689e5a92dd80e184bc1b7
perezfo <german.perez-fogwill@fmi.fi>
2022-06-22 14:57:11 +0300

    Create GUI for temperature control

---
+++ src/daq/gui_temperature_control.py
@@ -0,0 +1,55 @@
+import sys
+import random
+import matplotlib
+
+matplotlib.use('Qt5Agg')
+
+from PyQt5 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+from src.daq import io
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.setCentralWidget(self.canvas)
+
+        self.daq = io.Daq()
+
+        self.bath_temp = []
+        self.setpoint = []
+        self.update_plot()
+
+        self.show()
+
+        # Setup a timer to trigger the redraw by calling update_plot.
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        # Drop off the first y element, append a new one.
+        self.bath_temp.append(self.daq.get_bath_temp())
+        self.canvas.axes.cla()  # Clear the canvas.
+        self.canvas.axes.plot(self.bath_temp, 'r')
+        # Trigger the canvas to update and redraw.
+        self.canvas.draw()
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+app.exec_()
