9972a4cfc8e9cfea875922a60d5e75a4d9bcf2f7
perezfo <german.perez-fogwill@fmi.fi>
2022-06-28 09:46:07 +0300

    style

--- src/daq/gui_temperature_control.py
+++ src/daq/gui_temperature_control.py
@@ -6,17 +6,7 @@ matplotlib.use('Qt5Agg')
 
 from PyQt5 import QtCore, QtWidgets
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-
 from src.daq import io
-
-
-class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -24,8 +14,10 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
-        self.setCentralWidget(self.canvas)
+        window = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(QtWidgets.QPushButton('Top'))
+        layout.addWidget(QtWidgets.QPushButton('Bottom'))
 
         self.daq = io.Daq()
 
@@ -33,6 +25,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setpoint = []
         self.update_plot()
 
+        window.setLayout(layout)
         self.show()
 
         # Setup a timer to trigger the redraw by calling update_plot.
