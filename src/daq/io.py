4bb95eeaeef2f7ab63aa5c1858b26676c841be54
perezfo <german.perez-fogwill@fmi.fi>
2022-06-28 09:43:31 +0300

    feat: multiple changes in temperature control
    - Set temperature at startup in AOUT0
    - Change set_temperature function so that reads the setpoint temp and until this value is not reached, the temperature is changed
    - fix temperatures multiplier

--- src/daq/io.py
+++ src/daq/io.py
@@ -20,41 +20,51 @@ class Daq:
             self.ai = self.daq_device.get_ai_device()
             self.ai.info = self.ai.get_info()
 
-            self.temp_setpoint = 16
-
         except ULException as e:
             print('\n', e)
 
     def set_reference_voltage(self):
+        self.ao.a_out(channel=1, analog_range=Range.BIP10VOLTS, flags=AOutFlag.DEFAULT, data=9)
+        self.ao.a_out(channel=0, analog_range=Range.BIP10VOLTS, flags=AOutFlag.DEFAULT, data=17*10e-3)
 
-        self.ao.a_out(channel=1,
-                      analog_range=Range.BIP10VOLTS,
-                      flags=AOutFlag.DEFAULT,
-                      data=9)
+    def set_temperature(self, t_target):
+        v_aout = t_target * 10.0e-3
 
-    def set_temperature(self, temp):
+        print(f'Value to be set in AOUT0: {v_aout}')
+        self.ao.a_out(channel=0, analog_range=Range.BIP10VOLTS, flags=AOutFlag.DEFAULT, data=v_aout)
 
-        self.ao.a_out(channel=0,
-                      analog_range=Range.BIP10VOLTS,
-                      flags=AOutFlag.DEFAULT,
-                      data=(temp - self.temp_setpoint) / 100)
+        # Check if setpoint is correct
+        t_setpoint = self.get_setpoint_temp()
+        t_diff = t_target - t_setpoint
+
+        while t_diff > 0.01:
+            v_aout = (t_target + t_diff) * 10.0e-3
+
+            print(f'Value to be set in AOUT0: {v_aout}')
+            self.ao.a_out(channel=0, analog_range=Range.BIP10VOLTS, flags=AOutFlag.DEFAULT, data=v_aout)
+
+            # Check if setpoint is correct
+            t_setpoint = self.get_setpoint_temp()
+            t_diff = t_target - t_setpoint
 
     def get_bath_temp(self, samples=100, interval=1e-3):
         tmp = []
+
         for i in range(samples):
             time.sleep(interval)
             a_in = self.ai.a_in(channel=5, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS,
                                 flags=AInFlag.DEFAULT)
             tmp.append(a_in)
 
-        return sum(tmp) / len(tmp)
+        return (sum(tmp) / len(tmp)) * 100
 
     def get_setpoint_temp(self, samples=100, interval=1e-3):
         tmp = []
+
         for i in range(samples):
             time.sleep(interval)
             a_in = self.ai.a_in(channel=4, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS,
                                 flags=AInFlag.DEFAULT)
             tmp.append(a_in)
 
-        return sum(tmp) / len(tmp)
+        return (sum(tmp) / len(tmp)) * 100
