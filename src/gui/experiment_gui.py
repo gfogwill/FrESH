d13cb380b06c1838c8e7bf030b1be0399e021f8c
DASI <dasi@fmi.fi>
2022-09-29 14:54:55 +0300

    fix: save experiment

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -241,7 +241,7 @@ class ExperimentUi(QtWidgets.QMainWindow):
     def set_temp(self):
         t = float(self.temp_set.text())
         logging.info(f'Setting temperature to: {t}')
-        self.daq.set_temperature(t)
+        self.data_worker.daq.set_temperature(t)
 
     @pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
