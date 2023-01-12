d8431990a1492483af17c492f144d8d40ffba53c
perezfo <german.perez-fogwill@fmi.fi>
2023-01-12 14:46:51 +0200

    doc: document methods

--- src/daq/mccdaq.py
+++ src/daq/mccdaq.py
@@ -6,8 +6,8 @@ import logging
 
 class Daq:
     """
-    Daq class is used to interact with a USB-1808 DAQ device from MCCDAQ to perform functions such as setting the temperature and reading the bath temperature and setpoint temperature.
-
+    Daq class is used to interact with a USB-1808 DAQ device from MCCDAQ to perform functions such as setting the
+    temperature and reading the bath temperature and setpoint temperature.
     """
     def __init__(self):
         try:
@@ -32,11 +32,14 @@ class Daq:
         """
         Sets the starting temperature on channel 0 of the USB-1808 device.
 
-        :param t: float value representing the temperature in degree Celsius.
-        :type t: float
+        Parameters
+        ----------
+        t : float
+            float value representing the temperature in degree Celsius.
 
-        :Example:
-        daq.set_starting_temp(20)
+        Example
+        -------
+        set_starting_temp(20)
         """
 
         self.ao.a_out(channel=0, analog_range=Range.BIP10VOLTS, flags=AOutFlag.DEFAULT, data=t*10e-3)
@@ -45,12 +48,14 @@ class Daq:
         """
         Sets the temperature on channel 0 of the USB-1808 device.
 
-        :param t_target: float value representing the target temperature in degree Celsius.
-        :type t_target: float
+        Parameters
+        ----------
+        t_target : float
+            float value representing the target temperature in degree Celsius.
 
-        :Example:
-        daq.set_temperature(25)
-
+        Example
+        -------
+        set_temperature(25)
         """
 
         v_aout = t_target * 10.0e-3
@@ -76,10 +81,16 @@ class Daq:
         """
         Reads the bath temperature from channel 5 of the USB-1808 device.
 
-        :Example:
-        daq.read_bath_temp() -> 20.5
+        Returns
+        -------
+        float
+            float value representing the temperature in degree Celsius.
 
+        Example
+        -------
+        read_bath_temp() -> 20.5
         """
+
         a_in = self.ai.a_in(channel=5, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS, flags=AInFlag.DEFAULT)
 
         return a_in
@@ -88,13 +99,16 @@ class Daq:
         """
         Reads the setpoint temperature from channel 4 of the USB-1808 device.
 
-        :return: float value representing the temperature in degree Celsius.
-        :rtype: float
+        Returns
+        -------
+        float
+            float value representing the temperature in degree Celsius.
 
-        :Example:
-        daq.read_setpoint_temp() -> 22.3
-
+        Example
+        -------
+        read_setpoint_temp() -> 22.3
         """
+
         a_in = self.ai.a_in(channel=4, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS, flags=AInFlag.DEFAULT)
 
         return a_in
