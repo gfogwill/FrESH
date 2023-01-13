c795651afbd77a76f2458e5f5fbbcd5ad575a020
perezfo <german.perez-fogwill@fmi.fi>
2023-01-13 16:40:06 +0200

    refactor: create experiment form to enter metadata before running the experiment

--- src/gui/main_gui.py
+++ src/gui/main_gui.py
@@ -14,15 +14,17 @@ class MainUi(QtWidgets.QMainWindow):
 
         uic.loadUi('main.ui', self)
 
-        self.button_new_experiment = self.findChild(QtWidgets.QPushButton, 'newExperimentButton')  # Find the button
+        # Find and connect the button
+        self.button_new_experiment = self.findChild(QtWidgets.QPushButton, 'newExperimentButton')
         self.button_new_experiment.clicked.connect(self.start_experiment)
 
     def start_experiment(self):
         self.hide()
-        exp_metadata = {'station_name': self.stationComboBox.currentText(),
-                        'exp_description': self.descriptionPlainTextEdit.toPlainText()}
 
-        self.ExperimentUi = ExperimentUi(exp_metadata=exp_metadata)
+        # exp_metadata = {'station_name': self.stationComboBox.currentText(),
+        #                 'exp_description': self.descriptionPlainTextEdit.toPlainText()}
+
+        self.ExperimentUi = ExperimentUi()
         self.ExperimentUi.show()
 
 
