b99dd4cc2887b2d03bcddb4623cb40c06f916df3
perezfo <german.perez-fogwill@fmi.fi>
2023-01-16 12:40:58 +0200

    style

--- src/daq/mccdaq.py
+++ src/daq/mccdaq.py
@@ -91,7 +91,10 @@ class Daq:
         read_bath_temp() -> 20.5
         """
 
-        a_in = self.ai.a_in(channel=5, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS, flags=AInFlag.DEFAULT)
+        a_in = self.ai.a_in(channel=5,
+                            input_mode=AiInputMode.DIFFERENTIAL,
+                            analog_range=Range.BIP10VOLTS,
+                            flags=AInFlag.DEFAULT)
 
         return a_in
 
@@ -109,7 +112,10 @@ class Daq:
         read_setpoint_temp() -> 22.3
         """
 
-        a_in = self.ai.a_in(channel=4, input_mode=AiInputMode.DIFFERENTIAL, analog_range=Range.BIP10VOLTS, flags=AInFlag.DEFAULT)
+        a_in = self.ai.a_in(channel=4,
+                            input_mode=AiInputMode.DIFFERENTIAL,
+                            analog_range=Range.BIP10VOLTS,
+                            flags=AInFlag.DEFAULT)
 
         return a_in
 
