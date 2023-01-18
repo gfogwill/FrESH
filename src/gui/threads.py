acc66dbb2aefd0e39f93884a1f02bd87d438b4e1
perezfo <german.perez-fogwill@fmi.fi>
2023-01-18 09:01:40 +0200

    style

--- src/gui/threads.py
+++ src/gui/threads.py
@@ -164,15 +164,6 @@ class VideoThread(QThread):
             ret, cv_img = self.cap.read()
             cv_img = cv2.rotate(cv_img, cv2.ROTATE_180)
             if ret:
-            #     cv2.putText(cv_img, f"     Bath temp: {self.bath_temp_text}",
-            #                 (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-            #     cv2.putText(cv_img, f" Setpoint temp: {self.setpoint_temp_text}",
-            #                 (50, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-            #     cv2.putText(cv_img, f"ADAM CH1 temp: {self.ADAMCH0_temp_text}",
-            #                 (50, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-            #     cv2.putText(cv_img, f"ADAM CH2 temp: {self.ADAMCH1_temp_text}",
-            #                 (50, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-
                 if get_circles:
                     cv_img = get_circles(cv_img, self.plot_circles)
 
