f8c39864d4405cfb11f9165a5705f55a8991921b
perezfo <german.perez-fogwill@fmi.fi>
2023-02-09 14:53:06 +0200

    refactor: search circles with random params

--- src/circles.py
+++ src/circles.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pathlib
 import os
+import random
 
 
 def sort_circles(circles, n_cols):
@@ -46,7 +47,7 @@ def plot_detected_circles(img, circles):
 
         for n, i in enumerate(circles):
             # outer circle
-            ## cv2.circle(image, center_coordinates, radius, color, thickness)
+            # cv2.circle(image, center_coordinates, radius, color, thickness)
             cv2.circle(img, (i[0], i[1]), i[2], (0, 0, 0), 1)
 
             # inner circle
@@ -62,16 +63,16 @@ def plot_detected_circles(img, circles):
 def get_grayscales(image, circles, mask=True):
     grayscales = []
 
-    for circle in circles[:96]:
+    for circle in circles:  # [:95]:
         x = circle[0]
         y = circle[1]
-        r = circle[2]
+        r = circle[2] - 5
 
         img = image[y - r:y + r, x - r:x + r]
 
         if mask:
             # create a mask
-            ## https://stackoverflow.com/questions/50697179/opencv-and-python-how-croped-circle-area-only
+            # https://stackoverflow.com/questions/50697179/opencv-and-python-how-croped-circle-area-only
 
             m = np.full((img.shape[0], img.shape[1]), 0, dtype=np.uint8)
             # create circle mask, center, radius, fill color, size of the border
@@ -89,17 +90,23 @@ def get_grayscales(image, circles, mask=True):
     return grayscales
 
 
-def get_circles(img, minDist=20, param1=60, param2=10, minRadius=10, maxRadius=13, sort=True, plot=True):
+def get_circles(img, minDist=40, param1=150, param2=15, minRadius=19, maxRadius=22, sort=True, plot=True):
     # https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
-    circles = cv2.HoughCircles(img,
-                               cv2.HOUGH_GRADIENT,
-                               1,
-                               minDist=minDist,
-                               param1=param1,
-                               param2=param2,
-                               minRadius=minRadius,
-                               maxRadius=maxRadius
-                               )[0]
+
+    n_circs = None
+
+    while n_circs != 96:
+        circles = cv2.HoughCircles(img,
+                                   cv2.HOUGH_GRADIENT,
+                                   1,
+                                   minDist=minDist,
+                                   param1=param1 + random.randint(-30, 30),
+                                   param2=param2 + random.randint(-5, 5),
+                                   minRadius=minRadius,
+                                   maxRadius=maxRadius
+                                   )[0]
+
+        n_circs = circles.shape[0]
 
     if sort:
         circles = sort_circles(circles, n_cols=12)
