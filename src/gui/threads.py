f801fb5ff4e07bccd144f770f201c48d2b7a9de9
DASI <dasi@fmi.fi>
2023-02-09 09:11:07 +0200

    style

--- src/gui/threads.py
+++ src/gui/threads.py
@@ -85,7 +85,7 @@ class TempThread(QThread):
         if self.chilling:
             self.last_sp -= self.chill_temp_step
             self.last_sp = round(self.last_sp, 2)
-            if self.last_sp == -10.0:
+            if self.last_sp == -30.0:
                 self.chilling = False
         else:
             self.last_sp += self.cool_temp_step
