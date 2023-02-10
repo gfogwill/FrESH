c8641b548eea4975cd45f4fc4ee5da1a342c02fe
perezfo <german.perez-fogwill@fmi.fi>
2023-02-10 11:02:37 +0200

    style: change gui. displat temps in dispaly

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -241,9 +241,9 @@ class ExperimentUi(QtWidgets.QMainWindow):
         self.line3.setData(*zip(*self.adam0))
         self.line4.setData(*zip(*self.adam1))
 
-        self.lcdBT.display(*self.bath_temp[-1)
-        self.lcdSP.display(*self.setpoint[-1)
-        self.lcdRTD1.display(*self.adam0[-1)
+        self.lcdBT.display(*self.bath_temp[-1])
+        self.lcdSP.display(*self.setpoint[-1])
+        self.lcdRTD1.display(*self.adam0[-1])
         self.lcdRTD2.display(*self.adam1[-1])
 
 
