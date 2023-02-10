e0c38a66acf623d9b2f05b19e968f164dbb69f2f
perezfo <german.perez-fogwill@fmi.fi>
2023-02-10 13:54:48 +0200

    style: change gui. display temps in dispaly

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -243,7 +243,6 @@ class ExperimentUi(QtWidgets.QMainWindow):
         self.lcdRTD1.display(str(f"{self.adam0[-1][1]:.2}"))
         self.lcdRTD2.display(str(f"{self.adam1[-1][1]:.2}"))
 
-        print(str(self.bath_temp[-1][1]))
 
     def set_temp(self):
         t = float(self.temp_set.text())
