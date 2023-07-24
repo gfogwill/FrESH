ec8f986842a4eb4bb0f86234a67babdb0d73bb6b
DASI <dasi@fmi.fi>
2023-07-24 10:23:52 +0300

    fix: image orentation (rotation)

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -285,7 +285,9 @@ class ExperimentUi(QtWidgets.QMainWindow):
     @pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
         """Updates the image_label with a new opencv image"""
+        #cv_img = cv2.rotate(cv_img, cv2.ROTATE_180)
         qt_img = convert_cv_qt(cv_img)
+
         self.image_frame.setPixmap(qt_img)
 
 
