fix: correction of the normalization factor

src/gui/analysis.py
@@ -183,7 +183,7 @@ class ExperimentAnalysisUi(QtWidgets.QMainWindow):
         self.FFwidget.plot(self.t, self.ff)
 
         # Normalization factor to L^-1
-        X = nu * v_wash / (v_air / filter_fraction)
+        X = nu * v_wash / (v_air * filter_fraction)
 
         # Concentration per sample
         self.conc_per_drop = - np.log(1 - np.array(self.ff)) / v_drop