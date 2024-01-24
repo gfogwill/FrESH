26371675e89e4421d302033e9ba516699689f50b
perezfo <german.perez-fogwill@fmi.fi>
2024-01-24 15:25:53 +0200

    chore: add temperature scan options

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -124,8 +124,8 @@ class ExperimentUi(QtWidgets.QMainWindow):
     def start_scan(self):
         max_temp = float(self.maxTemp.text())
         min_temp = float(self.minTemp.text())
-        cooling_rate = float(self.coolingRate.text())
-        heating_rate = float(self.heatingRate.text())
+        cooling_rate = float(self.coolingRate.text()/10)
+        heating_rate = float(self.heatingRate.text()/10)
         cycles = int(self.numCycles.text())
 
         self.temp_worker = TempThread(max_temp, min_temp, cooling_rate, heating_rate)
