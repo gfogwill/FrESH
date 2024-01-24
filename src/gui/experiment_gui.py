cbb8e0742c630e0fa2d63db032bd6d37bb13da50
perezfo <german.perez-fogwill@fmi.fi>
2024-01-24 13:30:34 +0200

    chore: add temperature scan options

--- src/gui/experiment_gui.py
+++ src/gui/experiment_gui.py
@@ -253,7 +253,7 @@ class ExperimentUi(QtWidgets.QMainWindow):
 
     def connect_chiller(self):
         # Setup thread for temperature I/O
-        self.data_worker = DataWorker(float(self.temp_set.text()))
+        self.data_worker = DataWorker(float(self.targetTemp.text()))
         self.data_worker.read_data_signal.connect(self.read_sensors_data)
         self.data_worker.start()
 
