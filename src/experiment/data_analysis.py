fixing nan values from background correction (nan values came from background normalisation factor retrieveal problem), retrieval of background normalisation factor is changed from experiment.py to data_analysis.py. This also fixed some metadata saving issues

src/experiment/data_analysis.py
@@ -9,6 +9,7 @@ Created on Wed Jul 20 09:09:00 2022
 import numpy as np
 from src import paths
 import os
+import json
 
 
 def uncertainty(ff, droplets, z):
@@ -83,19 +84,19 @@ def differential(counts, bin_size, show_info=False):
         #n = droplets - np.sum(counts[i:])
         if dn[i] == 0.0:
             if show_info:
-            	print('no droplets froze in bin with index', i)
+                print('no droplets froze in bin with index', i)
             k[i] = 0.0
         elif n[i] == 0.0:
             if show_info:
-            	print('no more unfrozen droplets at index', i)
+                print('no more unfrozen droplets at index', i)
             k[i] = 0.0 #- (1 / bin_size) * np.log(1 - (dn[i] / (n[i] + epsilon))) # Check if this is ok!!!
         elif dn[i]/n[i] == 1:
             k[i] = 0.0 
             if show_info:
-            	print('log goes to zero on index (dn/n=1)', i)
+                print('log goes to zero on index (dn/n=1)', i)
         elif 1 - (dn[i] / (n[i] + epsilon)) < 0.0:
             if show_info:
-            	print('dn > unfrozen droplets, something is wrong, check droplet count on index', i)
+                print('dn > unfrozen droplets, something is wrong, check droplet count on index', i)
             k[i] = 0.0
         else:
             k[i] = - (1 / bin_size) * np.log(1 - (dn[i] / (n[i])))
@@ -117,8 +118,13 @@ def initialize_background(background_exp, bin_size, z):
     diff_lower = differential(binned_bg['count_lower_conf_lvl'], bin_size)
     diff_upper = differential(binned_bg['count_upper_conf_lvl'], bin_size)
 
+    # retrieve the background experiment normalisation factor
+    with open(os.path.join(paths.raw_data_path, background_exp, 'metadata.json'), "r") as bgmetadata_file:
+        bg_metadata = json.load(bgmetadata_file)
+        normalisation_factor = float(bg_metadata.get("normalisation_factor", 1.0))  # Default to 1.0 if missing
+
     return binned_bg['temp'], binned_bg['ff'], binned_bg['ff_lower_conf_lvl'], binned_bg['ff_upper_conf_lvl'], \
-        diff, diff_lower, diff_upper
+        diff, diff_lower, diff_upper, normalisation_factor
 
 
 def bg_correction(BG_temp, BG_ff, BG_diff, BG_lower, BG_upper, sample_temp, sample_ff, sample_diff,
@@ -136,13 +142,14 @@ def bg_correction(BG_temp, BG_ff, BG_diff, BG_lower, BG_upper, sample_temp, samp
     for i, t in enumerate(sample_temp):
         # berform correction, if sample temperature bin is in the background temperature bins
         if t in BG_temp:
+            idx = np.where(np.isclose(BG_temp, t))[0][0]
             # if the difference is smaller than 0, set corrected value to zero
-            if (sample_diff[i] - BG_diff[BG_temp == t]) < 0:
+            if (sample_diff[i] - BG_diff[idx]) < 0:
                 if show_info:
-                	print(f'below zero value in bg correction at temperature {t}')
+                    print(f'below zero value in bg correction at temperature {t}')
                 corrected_diff[i], corrected_lower[i], corrected_upper[i] = 0, 0, 0
             # if frozen fraction of the sample has lower values than bg, set corrected value to zero
-            elif sample_ff[i] < BG_ff[BG_temp == t]:
+            elif sample_ff[i] < BG_ff[idx]:
                 if show_info:
                     print(f'sample ff lower than background at temperature {t}!')
                 corrected_diff[i], corrected_lower[i], corrected_upper[i] = 0, 0, 0
@@ -150,10 +157,9 @@ def bg_correction(BG_temp, BG_ff, BG_diff, BG_lower, BG_upper, sample_temp, samp
             else:
                 if show_info:
                     print(f'Background corrected at temp {t}')
-                    print(sample_diff[i], BG_diff[BG_temp == t])
-                corrected_diff[i] = sample_diff[i] - BG_diff[BG_temp == t][0]
-                corrected_lower[i] = np.sqrt(sample_lower[i]**2 + BG_lower[BG_temp == t]**2)
-                corrected_upper[i] = np.sqrt(sample_upper[i]**2 + BG_upper[BG_temp == t]**2)
+                corrected_diff[i] = sample_diff[i] - BG_diff[idx]
+                corrected_lower[i] = np.sqrt(sample_lower[i]**2 + BG_lower[idx]**2)
+                corrected_upper[i] = np.sqrt(sample_upper[i]**2 + BG_upper[idx]**2)
         # if sample temperatures are lower than minimum background temperature result is 0
         elif t < np.min(BG_temp):
             if show_info:
@@ -161,13 +167,15 @@ def bg_correction(BG_temp, BG_ff, BG_diff, BG_lower, BG_upper, sample_temp, samp
             corrected_diff[i], corrected_lower[i], corrected_upper[i] = 0, 0, 0
         # if sample temperature bin is not in the background temperature bins, differential is unchanged
         else:
+            if show_info:
+                print(f'no background correction needed at temperature {t}')
             corrected_diff[i], corrected_lower[i], corrected_upper[i] = sample_diff[i], sample_lower[i], sample_upper[i]
 
     # return  as confidence levels
     return corrected_diff, corrected_diff - corrected_lower, corrected_diff + corrected_upper
 
 
-def spectra(freezing_temps, X, Y, bin_size, z, background_exp, X_bg=1, depression=0):
+def spectra(freezing_temps, X, Y, bin_size, z, background_exp, depression=0):
     # X is normalisation factor befor background and Y normalisation after ie
     """ calculate spectra to processed data with background correction
     note: all normalisation factors are handled in this function """
@@ -196,7 +204,7 @@ def spectra(freezing_temps, X, Y, bin_size, z, background_exp, X_bg=1, depressio
         
     else:
         # calculate background correction to differential spectrum, normalise background to droplet volume
-        bg_temp, bg_ff, bg_ff_lower, bg_ff_upper, bg_diff, bg_diff_lower, bg_diff_upper = \
+        bg_temp, bg_ff, bg_ff_lower, bg_ff_upper, bg_diff, bg_diff_lower, bg_diff_upper, X_bg = \
             initialize_background(background_exp, bin_size, z)
         # within the input, normalise the bg differential to bg droplet volume
         diff, diff_lower, diff_upper = bg_correction(bg_temp, bg_ff, bg_diff * X_bg,

src/experiment/experiment.py
@@ -130,7 +130,7 @@ class ExperimentMetadata:
         self.latitude = kwargs.get('latitude', None)
         self.longitude = kwargs.get('longitude', None)
         # adding info on the normalisation factor and resulting units
-        self.normalisation_factor = kwargs.get('normalisation factor', None)
+        self.normalisation_factor = kwargs.get('normalisation_factor', None)
         self.units = kwargs.get('units', 'L-1')
 
 
@@ -175,6 +175,7 @@ class FrESHExperiment:
     def __init__(self, experiment_name):
         self.exp_name = experiment_name
         self.metadata = None
+        self.bg_metadata = None
 
         self.is_analyzed = False
         self.background_corrected = False
@@ -238,14 +239,8 @@ class FrESHExperiment:
 
     def run_analysis(self, existing_freezing_idxs=None):
 
-        #self.metadata.scan_start_timestamp = None
-        #self.metadata.scan_end_timestamp = None
-
         img_file_list = self.get_experiment_image_list()
-        #print('\n image list', len(img_file_list), '\n')
-
         self.grayscales_evolution = self.process_images(img_file_list)
-        #print('\n image list', len(self.greyscales_evolution), '\n')
 
         if existing_freezing_idxs is None:
             self.freezing_idxs = calculate_freezing_idxs(self.grayscales_evolution)
@@ -263,12 +258,7 @@ class FrESHExperiment:
 
         self.t, self.ff = process_sensors_data(self.exp_name, self.freezing_idxs, freezing_times)
 
-        background_experiment_save = self.metadata.background_exp
-        if background_experiment_save!= 'None' and background_experiment_save is not None:
-            bg_metadata = self.load_background_metadata(background_experiment_save)
-            X_bg = bg_metadata.normalisation_factor
-        else:
-            X_bg = 1.0
+        #background_experiment_save = self.metadata.background_exp
 
         # X is normalisation applied before bg correction, sampled volume correction is applied after
 
@@ -283,8 +273,7 @@ class FrESHExperiment:
         self.conc_per_L = self.conc_per_drop / v_air
 
         self.spectra, self.background_corrected = spectra(self.freezing_temps, X, v_air, 0.5, 1.96,
-                                                          background_exp=background_experiment_save,
-                                                          X_bg=np.float64(X_bg), depression=0)
+                                                          self.metadata.background_exp, 0)
 
         self.is_analyzed = True
 
@@ -457,33 +446,6 @@ class FrESHExperiment:
         else:
             return None
 
-    def load_background_metadata(self, exp_name):
-        metadata_path = os.path.join(paths.raw_data_path, exp_name, "metadata.json")
-        if os.path.exists(metadata_path):
-            if os.path.getsize(metadata_path) == 0:  # Check if file is empty
-                print('file empty')
-                self.load_metadata_from_raw_file(self.exp_name)
-                return
-
-            with open(metadata_path, "r", encoding="utf-8-sig") as metadata_file:
-                try:
-                    content = metadata_file.read()
-                    content = content.replace('"None"', 'null')
-                    content = content.replace('""', 'null')
-                    metadata_dict = json.loads(content)
-                except json.JSONDecodeError as e:
-                    print('Error loading metadata ', e)
-                    #self.load_metadata_from_raw_file()
-                    return
-
-                metadata_dict = {k.lower(): v for k, v in metadata_dict.items()}
-                self.metadata = ExperimentMetadata(**metadata_dict)
-
-                # Validate and correct metadata fields
-                self.validate_and_correct_metadata()
-                return self.metadata
-        else:
-            return None
 
     def validate_and_correct_metadata(self):
         needs_reload = False